d0604ea3c9f5e60c028b8e7f1ef4d80437a24b79
Avinash Seth <tutsinhindi@gmail.com>
2016-06-15 20:33:45 +0600

    added files

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>For you guys</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+</head>
+<body>
+    Hello, world <i class="fa fa-check"></i>   
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
