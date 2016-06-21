6a082c6d0a1d86dfd892f4ab41219c35c9fa41e8
Avinash Seth <tutsinhindi@gmail.com>
2016-06-21 15:14:32 +0600

    part 7

--- register.php
+++ register.php
@@ -1,10 +1,13 @@
 <?php
     $register_button = $_POST["register_btn"];
     if(isset($register_button)) {
+        // process only when submit button is clicked
         $email = $_POST["email_address_of_user"];
         $password = $_POST["password_of_user"];
         if(isset($email) && !empty($email) && isset($password) && !empty($password)) {
-            echo "everthing ok";
+            // process only when both fields are filled and not empty
+            echo htmlentities($email);
+            echo htmlentities($password);
         } else {
             echo "both fields are required";
         }
