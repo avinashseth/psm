77f810ec89e0553c3bb053284d3e59a838f72670
Avinash Seth <tutsinhindi@gmail.com>
2016-06-22 12:32:57 +0600

    created branch part 8 and added code

--- application/constants.php
+++ application/constants.php
@@ -1,2 +1,5 @@
 <?php
     define('BASE_URL','http://localhost:8080/psm/');
+
+    // table name
+    define('TBL_OWNER_LOGIN_INFORMATION','owner_login_information');
--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+    require_once("application/db.php");
     $register_button = $_POST["register_btn"];
     if(isset($register_button)) {
         // process only when submit button is clicked
@@ -6,8 +7,18 @@
         $password = $_POST["password_of_user"];
         if(isset($email) && !empty($email) && isset($password) && !empty($password)) {
             // process only when both fields are filled and not empty
-            echo htmlentities($email);
-            echo htmlentities($password);
+            $sql = "INSERT INTO `".TBL_OWNER_LOGIN_INFORMATION."`";
+            $sql .= "(`owner_unique`, `owner_email`, ";
+            $sql .= "`owner_password`, ";
+            $sql .= "`status`, `verification`) VALUES ";
+            $sql .= "('123sadf123sdf','".htmlentities($email)."','".htmlentities($password)."',";
+            $sql .= ",0,'asdfdsasadfdsf')";
+            $result = mysql_query($sql);
+            if($result) {
+                echo "values inserted";
+            } else {
+                echo mysql_error();
+            }
         } else {
             echo "both fields are required";
         }
