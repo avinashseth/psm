886a5a942ec3a2045475d854517848a541c17a94
Avinash Seth <tutsinhindi@gmail.com>
2016-07-15 16:35:56 +0600

    completed

--- application/constants.php
+++ application/constants.php
@@ -1,6 +1,5 @@
 <?php
     require_once __DIR__ . '/../vendor/autoload.php';
-    require_once __DIR__ . '/../secret_passwords.php';
 
     $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
     $dotenv->load();
